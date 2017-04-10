f05c57c488ead6b2e21bacb4d0804cdf5ae1e935
Jonathan Elchison <JElchison@Gmail.com>
2017-04-10 17:54:34 -0400

    Move to general-purpose GENERATE_HASH_TOOL

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ pushd server
 
 docker build --build-arg PASSWORD="Always_use_TLS" --build-arg PASSWORD_FILE="/opt/crypto-lab/shared/password1.db" --build-arg MODE=bcrypt --build-arg HTTPS=False -t lab1_server .
 #docker build --build-arg PASSWORD="Always store passwords hashed and salted (never plain-text)" --build-arg PASSWORD_FILE="/opt/crypto-lab/shared/password2.db" --build-arg MODE=plaintext --build-arg HTTPS=True -t lab2_server .
-#docker build --build-arg PASSWORD="Always compare password hashes using constant-time comparison" --build-arg PASSWORD_FILE="/opt/crypto-lab/password3.db" --build-arg MODE=plaintext --build-arg HTTPS=True -t lab3_server .
+#docker build --build-arg PASSWORD="Always compare password hashes using constant-time comparison" --build-arg PASSWORD_FILE="/opt/crypto-lab/password.db" --build-arg MODE=plaintext --build-arg HTTPS=True -t lab3_server .
 #docker build --build-arg PASSWORD="bcrypt is the best (current) method for password storage" --build-arg PASSWORD_FILE="/opt/crypto-lab/shared/password4.db" --build-arg MODE=md5 --build-arg HTTPS=True -t lab4_server .
 
 docker run -d -p 5001:5000 -v /opt/crypto-lab/shared:/opt/crypto-lab/shared:ro lab1_server
