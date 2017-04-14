6ed116a061c4eedf440338ca84149d7be3671472
Jonathan Elchison <JElchison@Gmail.com>
2017-04-14 15:26:08 -0400

    Hide password in file contents so it doesn't appear in ps listing

--- client/client.sh
+++ client/client.sh
@@ -6,10 +6,14 @@ HOST=$1
 PORT=$2
 HTTPS=$3
 
+PASSWORD_FILE=nothing_to_see_here.txt
+
+echo "password=$PASSWORD" > $PASSWORD_FILE
+
 
 if [[ $HTTPS = "True" ]]; then
-    watch "curl -v -s -k -X POST -d 'password=$PASSWORD' https://$HOST:$PORT/"
+    watch "curl -v -s -k -X POST -d @$PASSWORD_FILE https://$HOST:$PORT/"
 else
-    watch "curl -v -s -X POST -d 'password=$PASSWORD' http://$HOST:$PORT/"
+    watch "curl -v -s -X POST -d @$PASSWORD_FILE http://$HOST:$PORT/"
 fi
 
