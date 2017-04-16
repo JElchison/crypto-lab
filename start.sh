6479bdc7eb73849ad9b66475ce7e462b51080966
Jonathan Elchison <JElchison@Gmail.com>
2017-04-16 14:30:54 -0400

    Attempt another method for letting student user use tcpdump

--- start.sh
+++ start.sh
@@ -94,13 +94,7 @@ fi
 chmod o-rx /home/ubuntu
 
 # allow student user to run tcpdump
-groupadd pcap || true
-usermod -a -G pcap ubuntu
-usermod -a -G pcap student
-chgrp pcap /usr/sbin/tcpdump
-chmod 750 /usr/sbin/tcpdump
-setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
-
+echo "user1  ALL=(ALL)       NOPASSWD: /usr/sbin/tcpdump" > /etc/sudoers.d/tcpdump
 
 #
 # build client
